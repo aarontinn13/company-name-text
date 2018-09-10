44547cb345fc1573770c7b602c4abc7e9e998861
Aaron <tinn.3@buckeyemail.osu.edu>
2018-09-10 18:08:53 -0500

    saving

--- requests_url.py
+++ requests_url.py
@@ -3,7 +3,7 @@ import clearbit
 
 clearbit.key = 'sk_6a79cb6eb22caf2e9478c3fc74cebf44'
 
-with open('file.txt', 'r') as r:
+with open('testfile.txt', 'r') as r:
     for line in r:
         url = ('https://company.clearbit.com/v1/domains/find?name={}'.format(line))
         response1 = requests.get(url, auth=(clearbit.key,'pass'))
@@ -11,6 +11,7 @@ with open('file.txt', 'r') as r:
 
         response1 = response1.json()
 
+        #currently writing to append
         with open('file2.txt', 'a') as a:
             try:
                 a.write(response1['domain'])
@@ -18,3 +19,4 @@ with open('file.txt', 'r') as r:
             except KeyError:
                 a.write('Cannot Find Website')
                 a.write('\n')
+
